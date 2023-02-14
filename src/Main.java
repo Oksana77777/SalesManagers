ae30c15eb35718a097fe2b5523f8a0355b8ee3c0
Oksana777777 <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>
2023-02-14 13:09:54 +0300

    SalesManager

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) {
 
-        SalesManager salesManager = new SalesManager(new int[5]);
-        System.out.println(salesManager.max());
+    public static void main(String[] args) {
+        int[] sales = new int[]{7, 8, 777, 888};
+        SalesManager salesManager = new SalesManager(sales);
+        int max = salesManager.max();
+        System.out.println(max);
 
     }
 }
