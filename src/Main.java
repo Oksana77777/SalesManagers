8b1f770f32ebcd51197849f0b852b337760afe81
Oksana777777 <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>
2023-02-14 12:27:49 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SalesManager salesManager = new SalesManager(new int[5]);
+        System.out.println(salesManager.max());
+
+    }
+}
+
