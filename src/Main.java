38cd6c2ae380e0ff286e18f91f3bcfaddda4a789
Oksana77777 <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>
2023-02-14 15:11:05 +0300

    Switch to long

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales = new int[]{7, 8, 777, 888};
+       long[] sales = new long[]{7, 8, 777, 888};
         SalesManager salesManager = new SalesManager(sales);
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
 
     }
